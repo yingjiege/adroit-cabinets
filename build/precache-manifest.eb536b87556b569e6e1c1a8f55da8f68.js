ce189167d8229fe1d983674e0b5d1ff0fff3143c
yingjiege <127132937+yingjiege@users.noreply.github.com>
2023-05-23 15:31:30 -0400

    Add files via upload

---
+++ build/precache-manifest.eb536b87556b569e6e1c1a8f55da8f68.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f20460acad9f2170831f",
+    "url": "/adroit-cabinets/static/js/main.f20460ac.chunk.js"
+  },
+  {
+    "revision": "d59c1fbc23e8b106b7e8",
+    "url": "/adroit-cabinets/static/css/1.2b403170.chunk.css"
+  },
+  {
+    "revision": "d59c1fbc23e8b106b7e8",
+    "url": "/adroit-cabinets/static/js/1.d59c1fbc.chunk.js"
+  },
+  {
+    "revision": "dd8c4ea3f83380657cf7",
+    "url": "/adroit-cabinets/static/js/runtime~main.dd8c4ea3.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/adroit-cabinets/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/adroit-cabinets/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/adroit-cabinets/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/adroit-cabinets/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/adroit-cabinets/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "6273eea56f99504773f93459cd7985b9",
+    "url": "/adroit-cabinets/index.html"
+  }
+];
