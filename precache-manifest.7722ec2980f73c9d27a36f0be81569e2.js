7460870b6a5ce433d8cce0413356e8e79e9e9a7f
unknown <1026509513ge@gmail.com>
2023-05-24 09:55:33 -0400

    Updates

---
+++ precache-manifest.7722ec2980f73c9d27a36f0be81569e2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "56feadb525b2f93f1dc8",
+    "url": "/adroit-cabinets/static/js/main.63f591f4.chunk.js"
+  },
+  {
+    "revision": "6b72b5f65619ec854df6",
+    "url": "/adroit-cabinets/static/js/runtime~main.cdfcae30.js"
+  },
+  {
+    "revision": "4b31a3fff2b966b56777",
+    "url": "/adroit-cabinets/static/css/2.43825c07.chunk.css"
+  },
+  {
+    "revision": "4b31a3fff2b966b56777",
+    "url": "/adroit-cabinets/static/js/2.36965d6b.chunk.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/adroit-cabinets/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/adroit-cabinets/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/adroit-cabinets/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/adroit-cabinets/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/adroit-cabinets/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "31b0b9237aff5a5d659b1565da59d8dd",
+    "url": "/adroit-cabinets/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/adroit-cabinets/precache-manifest.eb536b87556b569e6e1c1a8f55da8f68.js"
+  "/adroit-cabinets/precache-manifest.7722ec2980f73c9d27a36f0be81569e2.js"
 );
 
 workbox.clientsClaim();
