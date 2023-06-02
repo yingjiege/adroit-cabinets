a5884b40d9d6f70f0fcceaacd66ca6d903f81b90
yingjiege <127132937+yingjiege@users.noreply.github.com>
2023-06-02 16:34:51 -0400

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,7 @@ function App() {
   });
 
   return (
-      <div className="main-container">
+      <div className="main-container" id="main">
         <div id="main">
           <CreateArea info={info} />
         </div>
