71739885e9e652dff210fd864b486987f9986c61
yingjiege <127132937+yingjiege@users.noreply.github.com>
2023-07-14 14:51:06 -0400

    Update AccTableHead.jsx

--- src/components/shop/accessory/AccTableHead.jsx
+++ src/components/shop/accessory/AccTableHead.jsx
@@ -5,7 +5,7 @@ function AccTableHead() {
     <thead className="table-success ">
       <tr>
         <th className="text-center" style={{ width: "100px" }}>Accessory</th>
-        <th className="text-center">Copy</th>
+        <th className="text-center" style={{ width: "60px" }}>Copy</th>
         <th className="text-center" style={{ width: "80px" }}>Item#</th>
         <th className="text-center" style={{ width: "200px" }}>ACC-NO</th>
         <th className="text-center" style={{ width: "100px" }}>COLOR</th>
