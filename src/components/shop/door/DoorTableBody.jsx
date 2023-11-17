01e7137203ecd1e09bc288697ddae1a1a6a80df7
yingjiege <127132937+yingjiege@users.noreply.github.com>
2023-11-17 08:20:31 -0500

    Update DoorTableBody.jsx

--- src/components/shop/door/DoorTableBody.jsx
+++ src/components/shop/door/DoorTableBody.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-//import panelFinishList from "../../../panelFinish.js";
 import axios from "axios";
 import Cookies from "js-cookie";
 
@@ -32,8 +31,6 @@ function NewTableBody({
     //if is in range
     if ((item.width < 3 && item.height <3) || item.width > 48){
       setWidthValue(false);
-      console.log(item.width)
-      console.log(item.height)
     }
     else if (item.width <3 && item.height > 3){
       setWidthValue(true);
@@ -83,7 +80,7 @@ function NewTableBody({
   }, [item]);
 
   const retrieveDoor = () =>{
-    axios.get("https://us-east-1.aws.data.mongodb-api.com/app/application-0-hxfdv/endpoint/door")
+    axios.get("https://us-east-1.aws.data.mongodb-api.com/app/application-0-hxfdv/endpoint/get_cabinetFinish")
     .then(response=>{
         setPanelFinishList(response.data);
     })
@@ -107,23 +104,6 @@ function NewTableBody({
         ></i>
       </td>
       <td className="text-center" style={{ width: "2em" }}>{ItemNum + 1}</td>
-      <td>
-        <input
-          type="text"
-          list="data"
-          className="form-control"
-          style={{ width: "12em" }}
-          placeholder="select panel"
-          name="panelFinish"
-          value={item.panelFinish}
-          onChange={(event) => handleEditTwo(event, item.id, item)}
-        />
-        <datalist id="data">
-          {panelFinishList.map((item, key) => (
-            <option key={key} value={item.label} />
-          ))}
-        </datalist>
-      </td>
       <td>
         <input
           type="text"
@@ -235,11 +215,108 @@ function NewTableBody({
           value={item.miterCut}
           onChange={(event) => handleEditAllInOne(event, item.id, item)}
         >
-          <option defaultValue="None">None</option>
+          <option defaultValue="">None</option>
           <option value="Top">Top</option>
           <option value="1H">1H</option>
           <option value="Bot">Bot</option>
         </select>
+      </td>
+      <td>
+      <select
+        id="inlineFormCustomSelectPref"
+        className="form-select w-auto"
+        name="edge"
+        value={item.edge}
+        onChange={(event) => handleEditAllInOne(event, item.id, item)}
+      >
+        <option value="">None</option>
+        <option value="1L">1L</option>
+        <option value="1W">1W</option>
+        <option value="1L1W">1L1W</option>
+        <option value="2W">2W</option>
+        <option value="2L">2L</option>
+        <option value="2L1W">2L1W</option>
+        <option value="1L2W">1L2W</option>
+        <option value="4S">4S</option>
+        <option value="EDGE_SP1">EDGE_SP1</option>
+        <option value="EDGE_SP2">EDGE_SP2</option>
+        <option value="EDGE_SP3">EDGE_SP3</option>
+        <option value="EDGE_SP4">EDGE_SP4</option>
+        <option value="EDGE_ANGLE">EDGE_ANGLE</option>
+      </select>
+      </td>
+      <td>
+      <select
+        id="inlineFormCustomSelectPref"
+        className="form-select w-auto"
+        name="drill"
+        value={item.drill}
+        onChange={(event) => handleEditAllInOne(event, item.id, item)}
+      >
+        <option value="">None</option>
+        <option value="2L">2L</option>
+        <option value="2R">2R</option>
+        <option value="2L_T23">2L_T23</option>
+        <option value="2R_T23">2R_T23</option>
+        <option value="2R_2L">2R_2L</option>
+        <option value="2R_2L_T23">2R_2L_T23</option>
+        <option value="3L">3L</option>
+        <option value="3R">3R</option>
+        <option value="3L_T23">3L_T23</option>
+        <option value="3R_T23">3R_T23</option>
+        <option value="2R_3L">2R_3L</option>
+        <option value="3R_2L">3R_2L</option>
+        <option value="2R_3L_T23">2R_3L_T23</option>
+        <option value="3R_2L_T23">3R_2L_T23</option>
+        <option value="3R_3L">3R_3L</option>
+        <option value="3R_3L_T23">3R_3L_T23</option>
+        <option value="4L">4L</option>
+        <option value="4R">4R</option>
+        <option value="4R_4L">4R_4L</option>
+        <option value="4R_2L">4R_2L</option>
+        <option value="4R_3L">4R_3L</option>
+        <option value="3R_4L">3R_4L</option>
+        <option value="4R_4L_T23">4R_4L_T23</option>
+        <option value="4R_2L_T23">4R_2L_T23</option>
+        <option value="4R_3L_T23">4R_3L_T23</option>
+        <option value="2R_4L_T23">2R_4L_T23</option>
+        <option value="3R_4L_T23">3R_4L_T23</option>
+        <option value="CAM">CAM</option>
+        <option value="CAM_15">CAM_15</option>
+        <option value="CAM_TK">CAM_TK</option>
+        <option value="CAM4">CAM4</option>
+        <option value="HINGE_2H">HINGE_2H</option>
+        <option value="HINGE_3H">HINGE_3H</option>
+        <option value="HINGE_4H">HINGE_4H</option>
+        <option value="HINGE_5H">HINGE_5H</option>
+        <option value="HINGE_6H">HINGE_6H</option>
+        <option value="HINGE_2W">HINGE_2W</option>
+        <option value="DOOR_SLID">DOOR_SLID</option>
+        <option value="ANGLE_CUT">ANGLE_CUT</option>
+      </select>
+            </td>      
+            <td>
+            <select
+        id="inlineFormCustomSelectPref"
+        className="form-select w-auto"
+        name="custom"
+        value={item.custom}
+        onChange={(event) => handleEditAllInOne(event, item.id, item)}
+      >
+        <option value="">None</option>
+        <option value="SP_REQUEST">SP_REQUEST</option>
+        <option value="LB_SP1">LB_SP1</option>
+        <option value="LB_SP2">LB_SP2</option>
+        <option value="CAM_SP1">CAM_SP1</option>
+        <option value="CAM_SP2">CAM_SP2</option>
+        <option value="MITER_CUT">MITER_CUT</option>
+        <option value="MATCH_GRAIN">MATCH_GRAIN</option>
+        <option value="POCKET_HOLE1">POCKET_HOLE1</option>
+        <option value="POCKET_HOLE2">POCKET_HOLE2</option>
+        <option value="DOW+SRW_HOLE">DOW+SRW_HOLE</option>
+        <option value="DOWEL_HOLE">DOWEL_HOLE</option>
+        <option value="SCREW_HOLE">SCREW_HOLE</option>
+      </select>
       </td>
       <td>
         <input
