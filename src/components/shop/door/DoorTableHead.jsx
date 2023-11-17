9da6690cc8c6bea49c48915a9e863b15501c07ea
yingjiege <127132937+yingjiege@users.noreply.github.com>
2023-11-17 08:21:44 -0500

    Update DoorTableHead.jsx

--- src/components/shop/door/DoorTableHead.jsx
+++ src/components/shop/door/DoorTableHead.jsx
@@ -7,7 +7,6 @@ function TableHead() {
         <th className="text-center">Door</th>
         <th className="text-center">Copy</th>
         <th className="text-center">Item#</th>
-        <th className="text-center">Panel Finish</th>
         <th className="text-center">Panel ID</th>
         <th className="text-center">Qty</th>
         <th className="text-center">Width(inch)</th>
@@ -15,6 +14,9 @@ function TableHead() {
         <th className="text-center">Hinge Hole</th>
         <th className="text-center">Match grain</th>
         <th className="text-center">Miter cut</th>
+        <th className="text-center">Edge</th>
+        <th className="text-center">Drilling</th>
+        <th className="text-center">Custom</th>
         <th className="text-center">Price</th>
         <th className="text-center">Subtotal</th>
       </tr>
