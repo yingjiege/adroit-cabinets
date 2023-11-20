d5a47933e9b3c2b7b8b46ce73ba24db81119c5cf
yingjiege <127132937+yingjiege@users.noreply.github.com>
2023-11-20 16:43:49 -0500

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './LandingPage'
 import LoginPage from './login/LoginPage'
@@ -19,6 +19,48 @@ import Management from './shop/Management'
 import PriceManagement from './shop/priceManagement'
 
 export default function App() {
+    useEffect(() => {
+        // LiveChat script
+        window.__lc = window.__lc || {};
+        window.__lc.license = 16678152;
+
+        (function (n, t, c) {
+            function i(n) {
+                return e._h ? e._h.apply(null, n) : e._q.push(n);
+            }
+            var e = {
+                _q: [],
+                _h: null,
+                _v: '2.0',
+                on: function () {
+                    i(['on', c.call(arguments)]);
+                },
+                once: function () {
+                    i(['once', c.call(arguments)]);
+                },
+                off: function () {
+                    i(['off', c.call(arguments)]);
+                },
+                get: function () {
+                    if (!e._h) throw new Error("[LiveChatWidget] You can't use getters before load.");
+                    return i(['get', c.call(arguments)]);
+                },
+                call: function () {
+                    i(['call', c.call(arguments)]);
+                },
+                init: function () {
+                    var n = t.createElement('script');
+                    n.async = !0;
+                    n.type = 'text/javascript';
+                    n.src = 'https://cdn.livechatinc.com/tracking.js';
+                    t.head.appendChild(n);
+                },
+            };
+
+            !n.__lc.asyncInit && e.init();
+            n.LiveChatWidget = n.LiveChatWidget || e;
+        })(window, document, [].slice);
+    }, []);
     const [info, setInfo] = useState({})
     return (
         <Router>
@@ -42,6 +84,9 @@ export default function App() {
                     <Route path="/price_management" element={ <PriceManagement/> } />
 
                 </Routes>
+                <noscript>
+                <a href="https://www.livechat.com/chat-with/16678152/" rel="nofollow">Chat with us</a>, powered by <a href="https://www.livechat.com/?welcome" rel="noopener nofollow" target="_blank">LiveChat</a>
+            </noscript>
             </div>
         </Router>
     )
